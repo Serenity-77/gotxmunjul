29b7f84a9a4cdf8b0efa4d370b60c963e0bbe135
harianja <harianja@localhost.localdomain>
2022-11-11 11:30:55 +0700

    doc.go

--- doc.go
+++
@@ -1,8 +0,0 @@
-package bagudung
-
-import (
-    _ "github.com/serenity-77/bagudung/client"
-    _ "github.com/serenity-77/bagudung/logger"
-    _ "github.com/serenity-77/bagudung/utils"
-    _ "github.com/serenity-77/bagudung/worker"
-)
